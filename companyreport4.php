71080490a24fa2adc2da686c9195e1f37ae76cc0
Jangkoo <leejangkoo@gmail.com>
2016-09-20 23:14:39 +0800

    THI risk matrix

--- companyreport4.php
+++ companyreport4.php
@@ -265,8 +265,9 @@ td p{
             <td rowspan="1" colspan="1"> <?php echo $hzardsValue['name'];?> </td>
             <td rowspan="1" colspan="1">  <?php echo $hzardsValue['accident'];?></td>
             <td rowspan="1" colspan="1">   <?php echo wordwrap ($hzardsValue['risk_control'], 15, "\n", 1);?></td>
+            <td rowspan="1" colspan="1"> <?php echo $romans[$hzardsValue['likehood']];?> </td>
             <td rowspan="1" colspan="1"> <?php echo $romans[$hzardsValue['security']];?></td>
-            <td rowspan="1" colspan="1"> <?php echo $roman[$hzardsValue['likehood']];?> </td>
+
 
               <?php
             $RPNLabel = "-";$RPN_TWOLabel = "-";
